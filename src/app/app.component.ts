97b5bd42ae69cdb530b0851f22b0308faa7e6ac4
JuanG <juan.gsanchez13@gmail.com>
2023-06-27 19:00:11 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = '202304-FinalProjectFront-Juan-Garcia';
+}
